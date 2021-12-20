1907b30c21c4a6d7c951e21163f751ccfe5dc285
Shreyash <60093808+DrCybernotix@users.noreply.github.com>
2021-12-20 10:06:16 +0530

    Update SW_UI.py

--- SW_UI.py
+++ SW_UI.py
@@ -1,1 +1,22 @@
-import tinter as tk
+from tkinter import *
+from tkinter import messagebox, filedialog
+import os
+import shutil
+
+
+root = Tk()
+root.geometry("885x660")
+root.title("R3ND3R UI")
+root.config(background="#5D6D7E")
+
+
+def open_file():
+    file = filedialog.askopenfilename()
+    os.startfile(file)
+    messagebox.showinfo('Render your Model', file+" Model rendered successfully.")
+
+
+def delete_file():
+    file = filedialog.askopenfilename()
+    os.remove(file)
+    messagebox.showinfo('Delete your Model', file+"Model deletion successful.")
